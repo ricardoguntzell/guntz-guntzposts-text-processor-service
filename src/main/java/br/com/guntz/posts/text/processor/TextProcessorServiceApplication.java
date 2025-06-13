b245944fd1054c9fa78359408450984c6c022d62
guntz <rricardoguntzell@gmail.com>
2025-06-13 16:54:27 -0300

    feat: Inclusão de projeto spring

---
+++ src/main/java/br/com/guntz/posts/text/processor/TextProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.guntz.posts.text.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextProcessorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guntz/posts/text/processor/TextProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guntz.posts.text.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextProcessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
